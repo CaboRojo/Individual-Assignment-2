b611f599077fd553c5f02f5aad1e2a2ec50cecd4
Alexis Sanchez - El Pecoso <alexis.sanchez@student.ie.edu>
2024-06-27 10:36:12 +0200

    Initial commit

--- cdk_app/cdk_app_stack.py
+++ cdk_app/cdk_app_stack.py
@@ -1,19 +1,57 @@
+# test_imports.py
 from aws_cdk import (
-    # Duration,
+    aws_dynamodb as dynamodb,
+    aws_lambda as _lambda, 
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    RemovalPolicy,
+    CfnOutput
 )
 from constructs import Construct
 
+
+print("Imports successful")
+
 class CdkAppStack(Stack):
+
+    _lambda_code= """
+    def handler(event, context):
+        pass 
+    """
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        table = dynamodb.Table(
+            self, "Table",
+            partition_key=dynamodb.Attribute(
+                name="id",
+                type=dynamodb.AttributeType.STRING
+            ),
+            removal_policy=RemovalPolicy.DESTROY
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkAppQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # with open('assets/_lamda_code.py', 'r') as fd:
+        #     code = fd.read()
+
+        function = _lambda.Function(
+            self, "Function",
+            handler="_lambda_code.handler",
+            timeout=Duration.minutes(5),
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            function_name="query_ddb",
+            code=_lambda.Code.from_asset("assets/func_query_ddb"),
+            memory_size=256,
+            environment={"TABLE": table.table_name }
+        )
+
+        table.grant_read_data(function)
+
+        CfnOutput(
+            self, "OutputTableARN",
+            value=table.table_arn,
+            key="TableARN",
+        )
+
+
+    
